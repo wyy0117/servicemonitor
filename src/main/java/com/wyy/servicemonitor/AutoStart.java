75edcb006f5f81dc3e6ac52017d03cc02825a44f
wyy0117 <349093330@qq.com>
2020-04-10 16:34:26 +0800

    优化

--- src/main/java/com/wyy/servicemonitor/AutoStart.java
+++ src/main/java/com/wyy/servicemonitor/AutoStart.java
@@ -9,9 +9,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.List;
 import java.util.Timer;
@@ -34,16 +31,8 @@ public class AutoStart implements ApplicationRunner {
         List<Monitor> monitorList = monitorConfiguration.getMonitors();
         log.debug("monitor size = " + monitorList.size());
         monitorList.forEach(monitor -> {
-            MonitorTask monitorTask = null;
-            try {
-                monitorTask = new MonitorTask(monitor);
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            } catch (InvalidKeyException e) {
-                e.printStackTrace();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+            MonitorTask monitorTask = new MonitorTask(monitor);
+
             Timer timer = new Timer();
             timer.schedule(monitorTask, new Date(), monitor.getPeriod());
         });
